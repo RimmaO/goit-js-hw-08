44a4ef1e99088bb078c0821d1e6602d20528b9be
RimmaO <rimma_o@ukr.net>
2023-05-02 19:57:33 +0300

    add task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML('beforeend', addGalleryMarkup);
+// console.log(galleryList);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+//* використовуємо бібліотеку SimpleLightbox (підключена через термінал npm install simplelightbox (package.json -> dep -> "simplelightbox": "^2.13.0"))
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
