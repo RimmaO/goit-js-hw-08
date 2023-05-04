7790f6c0ccafc1cd6f5bb03afaa8c8627e01dede
RimmaO <rimma_o@ukr.net>
2023-05-04 11:40:26 +0300

    update tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const formRef = document.querySelector('form');
-const emailRef = document.querySelector('input');
-const messageRef = document.querySelector('textarea');
-const submitRef = document.querySelector('button');
-
 const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
 
-// updateFields();
+const formData = {};
 
-// function updateFields() {
-//   formRef.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || '';
-// }
+statusOfStorage();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+//* отримуєм значення поля зберігаєм значення поля в сховище та добавляєм тротл
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(formData));
+}
+
+//* відправка форми:очищаєм форму та очищаєм сховище
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.target.reset();
+  localStorage.removeItem('LOCALSTORAGE_KEY');
+  console.log(formData);
+}
+
+//* відстежуєм значення в формі, якщо стор була перезавантажена, і якщо там щось було то збережемо в формі, якщо ні - null (Метод Object.entries(obj) повертає масив записів, кожен елемент якого, буде ще один масив з 2-х елементів: імені властивості і значення цієї властивості з об'єкта obj.)
+function statusOfStorage() {
+  const formDataSavedText = JSON.parse(
+    localStorage.getItem('LOCALSTORAGE_KEY')
+  );
+
+  if (formDataSavedText) {
+    Object.entries(formDataSavedText).forEach(function ([name, value]) {
+      formData[name] = value;
+      formRef.elements[name].value = value;
+      //   console.log(formRef.elements);
+    });
+  }
+}
