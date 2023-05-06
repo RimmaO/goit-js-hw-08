8927a306bbbf8b8e7ce7074652be1ce7aabe8f9d
RimmaO <rimma_o@ukr.net>
2023-05-06 19:49:18 +0300

    update

--- src/js/04-bonus.js
+++ src/js/04-bonus.js
@@ -5,7 +5,9 @@ const formRef = document.querySelector('form');
 const inputsRef = document.querySelectorAll('input');
 
 const formData = {};
-
+statusOfStorage();
+// 1.Для збереження даних у localStorage використовуйте ключ "form-data".
+// 4.При надсиланні форми дані повинні бути збережені в localStorage.
 // отримуєм значення поля та зберігаєм дані в localStorage та добавляєм тротл - щоб сховище оновлювалось не частіше, ніж раз на 1секунду
 function onFormInput(event) {
   formData[event.target.name] = event.target.value;
@@ -13,6 +15,22 @@ function onFormInput(event) {
 }
 formRef.addEventListener('input', throttle(onFormInput, 1000));
 
+// 2.Для валідації поля "Адреса електронної пошти" використовуйте регулярний вираз.
+
+// 3.При завантаженні сторінки, якщо дані є в localStorage, вони мають бути автоматично заповнені у форму.
+function statusOfStorage() {
+  const formDataSavedText = JSON.parse(
+    localStorage.getItem('LOCALSTORAGE_KEY')
+  );
+
+  if (formDataSavedText) {
+    Object.entries(formDataSavedText).forEach(function ([name, value]) {
+      formData[name] = value;
+      formRef.elements[name].value = value;
+    });
+  }
+}
+// 5.Додайте перевірку заповнення всіх полів форми перед збереженням в localStorage.
 // перевірка на заповненість ВСІХ полів відправка форми: очищаєм форму та очищаєм сховище
 function onFormSubmit(event) {
   event.preventDefaut();
@@ -24,3 +42,5 @@ function onFormSubmit(event) {
   console.log(formData);
 }
 formRef.addEventListener('submit', onFormSubmit);
+
+// 6.Додайте функціональність очищення даних із localStorage при натисканні на кнопку "Очистити".
